e37c6156e3121832f3accf3c2d397945071063a7
Peter Buckley <pbuckley@users.noreply.github.com>
2023-09-01 14:15:40 -0400

    let us prepare a curl command on failure

--- .buildkite/scripts/random_pass_fail.sh
+++ .buildkite/scripts/random_pass_fail.sh
@@ -20,6 +20,6 @@ if [[ $(( ${my_rand_seed}  % 2)) -eq 0 ]]; then
 fi
 
 echo "FAILURE!"
-buildkite-agent annotate --style "error" --context "${CONTEXTIBOO}" "step_id: ${BUILDKITE_STEP_KEY} job_id: ${BUILDKITE_JOB_ID} :bk-status-failed: :whale:"
+buildkite-agent annotate --style "error" --context "${CONTEXTIBOO}" ":bk-status-failed: :whale: curl -H "Authorization: Bearer ${BK_REST_API_TOKEN}" -X PUT "https://api.buildkite.com/v2/organizations/${BUILDKITE_ORGANIZATION_SLUG}/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds/${BUILDKITE_BUILD_NUMBER}/jobs/${BUILDKITE_JOB_ID}/retry""
 exit 1
 
