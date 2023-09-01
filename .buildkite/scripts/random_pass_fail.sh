35163a113f1f78db25305736fa21db5b92bed272
Peter Buckley <pbuckley@users.noreply.github.com>
2023-09-01 14:29:32 -0400

    fixup the env var by quoting, I just want to reuse

--- .buildkite/scripts/random_pass_fail.sh
+++ .buildkite/scripts/random_pass_fail.sh
@@ -9,6 +9,7 @@ export CONTEXTIBOO="ctx-${BUILDKITE_STEP_ID}-${my_rand_seed}"
 echo "Context: ${CONTEXTIBOO}"
 echo "STEP_KEY: ${BUILDKITE_STEP_KEY}"
 echo "STEP_ID: ${BUILDKITE_STEP_ID}"
+echo "JOB_ID: ${BUILDKITE_JOB_ID}"
 echo "NUM SHARDS IN BUILD.ENV: ${1}"
 
 # exit 0 on an even random number, exit 1 otherwise
@@ -20,6 +21,6 @@ if [[ $(( ${my_rand_seed}  % 2)) -eq 0 ]]; then
 fi
 
 echo "FAILURE!"
-buildkite-agent annotate --style "error" --context "${CONTEXTIBOO}" ":bk-status-failed: :whale: curl -H \"Authorization: Bearer ${BK_REST_API_TOKEN}\" -X PUT \"https://api.buildkite.com/v2/organizations/${BUILDKITE_ORGANIZATION_SLUG}/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds/${BUILDKITE_BUILD_NUMBER}/jobs/${BUILDKITE_JOB_ID}/retry\""
+buildkite-agent annotate --style "error" --context "${CONTEXTIBOO}" ":bk-status-failed: :whale: curl -H \"Authorization: Bearer \${BK_REST_API_TOKEN}\" -X PUT \"https://api.buildkite.com/v2/organizations/${BUILDKITE_ORGANIZATION_SLUG}/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds/${BUILDKITE_BUILD_NUMBER}/jobs/${BUILDKITE_JOB_ID}/retry\""
 exit 1
 
