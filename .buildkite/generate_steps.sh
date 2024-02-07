e13db1f62b331c60ab0c8a201162bd68ef1b04c2
Peter Buckley <pbuckley@users.noreply.github.com>
2024-02-07 08:25:40 -0500

    back to hello world no random pass fail

--- .buildkite/generate_steps.sh
+++ .buildkite/generate_steps.sh
@@ -55,8 +55,8 @@ EOF
 
   pass-fail)
     action_step=$(cat <<EOF
-  - label: ":zap: Shard %N of %t"
-    command: "bash .buildkite/scripts/random_pass_fail.sh"
+  - label: ":zap: Parallel job %N of %t"
+    command: "echo 'Hello, world!'"
     parallelism: 5 
 EOF
 )
