88e4ffd0193f26ce5c8778747ec06b5ec5bb48bc
Peter Buckley <pbuckley@users.noreply.github.com>
2024-10-06 22:46:45 -0400

    switch to ruamel but lose pipe chars oof

--- .buildkite/oswin-dynamic.py
+++ .buildkite/oswin-dynamic.py
@@ -1,31 +1,9 @@
-import yaml
+from ruamel.yaml import YAML
 import json
-from collections import OrderedDict
 
-
-class LiteralString(str):
-    pass
-
-
-class QuotedString(str):
-    pass
-
-
-def literal_string_representer(dumper, data):
-    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
-
-
-def quoted_string_representer(dumper, data):
-    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
-
-
-def ordered_dict_representer(dumper, data):
-    return dumper.represent_mapping('tag:yaml.org,2002:map', data.items())
-
-
-yaml.add_representer(LiteralString, literal_string_representer)
-yaml.add_representer(QuotedString, quoted_string_representer)
-yaml.add_representer(OrderedDict, ordered_dict_representer)
+yaml = YAML()
+yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.preserve_quotes = True
 
 
 def load_json_variables(file_path):
@@ -34,7 +12,7 @@ def load_json_variables(file_path):
 
 
 def format_commands(commands):
-    return LiteralString('\n'.join(commands) + '\n')  # Add an extra newline
+    return '\n'.join(commands) + '\n'  # Add an extra newline
 
 
 def format_plugins(plugins):
@@ -42,11 +20,9 @@ def format_plugins(plugins):
     for plugin in plugins:
         name = list(plugin.keys())[0]
         properties = plugin[name]
-        formatted_plugin = OrderedDict()
-        formatted_plugin[QuotedString(name)] = {
-            k: QuotedString(v) if isinstance(v, str) else v
-            for k, v in properties.items()
-        }
+        formatted_plugin = {name: {
+            k: v for k, v in properties.items()
+        }}
         formatted_plugins.append(formatted_plugin)
     return formatted_plugins
 
@@ -54,19 +30,19 @@ def format_plugins(plugins):
 def generate_yaml_data(items):
     people = []
     for item in items:
-        person = OrderedDict([
-            ('label', QuotedString(item['label'])),
-            ('key', QuotedString(item['key'])),
-            ('commands', format_commands(item['commands'])),
-            ('plugins', format_plugins(item['plugins']))
-        ])
+        person = {
+            'label': item['label'],
+            'key': item['key'],
+            'commands': format_commands(item['commands']),
+            'plugins': format_plugins(item['plugins'])
+        }
         people.append(person)
     return {'people': people}
 
 
 def save_yaml(data, file_path):
     with open(file_path, 'w') as file:
-        yaml.dump(data, file, default_flow_style=False, sort_keys=False)
+        yaml.dump(data, file)
 
 
 def main():
